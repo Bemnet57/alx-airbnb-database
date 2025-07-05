1ec14b074c87d2f85701cc300224c29bed46df52
Bemnet Asseged Bezabih <bemnetasseged57@gmail.com>
2025-07-05 22:06:18 +0300

    fixed ROWNUMBER() requirement

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,8 +7,6 @@ FROM User u
 LEFT JOIN Booking b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.first_name, u.last_name;
 
-
-
 SELECT 
     p.property_id,
     p.name AS property_name,
@@ -18,3 +16,17 @@ FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name;
 
+SELECT 
+    property_id,
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number_rank
+FROM (
+    SELECT 
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM Property p
+    LEFT JOIN Booking b ON p.property_id = b.property_id
+    GROUP BY p.property_id, p.name
+) AS booking_counts;
