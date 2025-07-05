972b17345cdf6fc1f080e49103d3306535b77b42
Bemnet Asseged Bezabih <bemnetasseged57@gmail.com>
2025-07-05 22:25:48 +0300

    added ["WHERE", "AND"]

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -62,3 +62,23 @@ LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
 -- JOIN Property p ON b.property_id = p.property_id
 -- LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
 
+-- ======================================
+-- Optimized Query (includes WHERE and AND )
+-- ======================================
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.start_date >= '2024-01-01'
+  AND b.status = 'confirmed';
+
+
